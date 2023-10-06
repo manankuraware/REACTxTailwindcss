036c3ef779dd97b6e79d1adbe8dc8cb17372d621
manankuraware <manankuraware2017@gmail.com>
2023-10-06 10:33:42 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neutralSilver': '#F5F7FA',
+        'neutralDGrey': '#4D4D4D',
+        'brandPrimary': '#4CAF4F',
+        'neutralGrey': '#717171',
+        'gray900': '#18191F',
+      }
+    },
   },
   plugins: [],
 }
