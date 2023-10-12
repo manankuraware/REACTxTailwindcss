2d100fdb4014f3ac458836310ee773907e351503
manankuraware <manankuraware2017@gmail.com>
2023-10-12 21:42:16 +0530

    another commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      {/* about text  */}
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
